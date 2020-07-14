a14b9105861f633e40740c86d47436c9021b820f
Krzysztof Gwóźdź <krzysztof.gwozdz@outlook.com>
2020-07-14 16:30:10 +0200

    Revert "Remove unnecessary Json attributes from CosmosDB documents", There is a bug in CosmosDb Sdk. These attributes are necessary.
    This reverts commit f658b7c0b0b38b793075c9fee3a30cdc81e61c87.

--- QuizApp.Infrastructure/CosmosDb/Documents/QuestionDocument.cs
+++ QuizApp.Infrastructure/CosmosDb/Documents/QuestionDocument.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace QuizApp.Infrastructure.CosmosDb.Documents
 {
 	public class QuestionDocument
 	{
+		[JsonProperty("id")]
 		public Guid Id { get; set; }
+
+		[JsonProperty("text")]
 		public string Text { get; set; }
+
+		[JsonProperty("answers")]
 		public ISet<AnswerDocument> Answers { get; set; }
+
+		[JsonProperty("tags")]
 		public string[] Tags { get; set; }
+
+		[JsonProperty("correctAnswersCount")]
 		public int CorrectAnswersCount { get; set; }
+
+		[JsonProperty("allAnswersCount")]
 		public int AllAnswersCount { get; set; }
 
 		public QuestionDocument(Guid id, string text, ISet<AnswerDocument> answers, string[] tags, int correctAnswersCount, int allAnswersCount)
@@ -24,8 +36,13 @@ namespace QuizApp.Infrastructure.CosmosDb.Documents
 
 		public class AnswerDocument
 		{
+			[JsonProperty("id")]
 			public Guid Id { get; set; }
+
+			[JsonProperty("text")]
 			public string Text { get; set; }
+
+			[JsonProperty("isCorrect")]
 			public bool IsCorrect { get; set; }
 
 			public AnswerDocument(Guid id, string text, bool isCorrect)
--- QuizApp.Infrastructure/CosmosDb/Documents/UserDocument.cs
+++ QuizApp.Infrastructure/CosmosDb/Documents/UserDocument.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace QuizApp.Infrastructure.CosmosDb.Documents
 {
 	public class UserDocument
 	{
+		[JsonProperty("id")]
 		public Guid Id { get; set; }
+
+		[JsonProperty("username")]
 		public string Username { get; set; }
+
+		[JsonProperty("passwordHash")]
 		public string PasswordHash { get; set; }
+
+		[JsonProperty("salt")]
 		public byte[] Salt { get; set; }
 
 		public UserDocument(Guid id, string username, string passwordHash, byte[] salt)
